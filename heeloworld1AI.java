92b5c92fa8dde07ca92454ecc426c88d3953db92
Rohan Kumar Sharma <127482666+Rohan1030@users.noreply.github.com>
2025-08-01 21:21:01 +0530

    done

---
+++ heeloworld1AI.java
@@ -0,0 +1,9 @@
+```java
+// This program prints "Hello, World!" to the console.
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print the message to the console
+        System.out.println("Hello, World!");
+    }
+}
+```
